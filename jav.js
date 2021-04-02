062a41fbe15cad6b3af2d29533669cdb9b3c9f04
ponnaganti963 <79781482+ponnaganti963@users.noreply.github.com>
2021-04-02 18:31:39 +0530

    Update jav.js

--- jav.js
+++ jav.js
@@ -1,34 +1,67 @@
-var parameters = location.search.substring(1).split("&");
-
-if(parameters.length <3){
-  document.getElementById('cartId').innerHTML = "Your Cart Is empty"
+if(document.readyState == 'loading'){
+   document.addEventListener('DOMContentLoaded',ready)
+}else{
+  ready();
 }
-else{
+
+function ready(){
   var parameters = location.search.substring(1).split("&");
-  var imgscr = parameters[2].split("=");
-  var splitting = imgscr[1].split("/");
-  i = unescape(splitting.slice(-1));
-  document.querySelector('.imgsrc').src = i;
 
-
-  var heading = parameters[1].split("=");
-  k = unescape(heading[1]);
-  document.querySelector('.heading').innerHTML= k;
-
-  var price = parameters[3].split("=");
-  var p = unescape(price[1]);
-  document.querySelector('.price').innerHTML = "₹" + p;
-
-  var qty = parameters[4].split("=");
-  var q = unescape(qty[1]);
-  document.querySelector('#Qty').innerHTML = q ;
-
-  var s = p.split(",");
-  var z = "";
-  for(let i=0; i<s.length ; i++){
-    z += s[i];
+  if(parameters.length <3){
+    var text = document.getElementById('cartId') ;
+    text.innerHTML = "Your Cart Is empty";
   }
-  a = parseInt(z);
+  else{
+    var parameters = location.search.substring(1).split("&");
+    var imgscr = parameters[2].split("=");
+    var splitting = imgscr[1].split("/");
+    i = unescape(splitting.slice(-1));
+    document.querySelector('.imgsrc').src = i;
+
+
+    var heading = parameters[1].split("=");
+    k = unescape(heading[1]);
+    document.querySelector('.heading').innerHTML= k;
+
+    var price = parameters[3].split("=");
+    var p = unescape(price[1]);
+    document.querySelector('.price').innerHTML = "₹" + p;
+
+    var qty = parameters[4].split("=");
+    var q = unescape(qty[1]);
+    document.querySelector('#Qty').value = q ;
+    updateCartTotal();
+
+  }
+
+
+  document.querySelector('#Qty').addEventListener('change',function(){
+    if(isNaN(this.value) || this.value<1){
+      this.value = 1;
+    }
+    updateCartTotal();
+  });
+
+  var buy = document.querySelector('#buy');
+  buy.addEventListener('click',function(){
+
+      document.getElementById('cartId').innerHTML = "Your Cart Is empty"
+      document.querySelector('#Qty').value = 0;
+      document.querySelector('#subtotal').innerHTML = 0;
+     alert('Thanks for shopping!!!!');
+    
+
+  });
+
+
+}
+
+function updateCartTotal(){
+  var p = document.querySelector('.price').innerHTML;
+  var q = document.querySelector('#Qty').value;
+   m = p.replace("₹"," ");
+   s = m.replace(",","");
+  a = parseInt(s);
   b = parseInt(q);
  document.querySelector('#subtotal').innerHTML = a * b;
 
