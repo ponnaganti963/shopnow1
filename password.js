f80a5e909e9152d047fdb5c24a061901c589b7da
ponnaganti963 <79781482+ponnaganti963@users.noreply.github.com>
2021-04-03 19:57:22 +0530

    Update password.js

--- password.js
+++ password.js
@@ -2,13 +2,13 @@
 
 function validating(){
   var text = document.getElementById('validationserver01').value;
-  localStorage.setItem('username',text);
+  // localStorage.setItem('username',text);
 return true;
   }
 
 const passField = document.querySelector('#pass');
-const showBtn = document.querySelector('span i');
-const iTag = document.querySelector('i');
+const showBtn = document.querySelector('span .i0');
+const iTag = document.querySelector('.i0');
 showBtn.onclick = (() =>{
   if(passField.type === "password"){
     passField.type = "text";
